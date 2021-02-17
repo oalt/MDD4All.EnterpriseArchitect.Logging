87b7a14641345d362c359078b681ff999a4b4f4a
oalt <oa@lacheweg21.de>
2021-02-17 20:09:27 +0100

    Initial commit

---
+++ src/MDD4All.EnterpriseArchitect.Logging/EaPluginNlogConfigurator.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Copyright (c) MDD4All.de, Dr. Oliver Alt
+ */
+using NLog;
+using NLog.Config;
+using EAAPI = EA;
+
+namespace MDD4All.EnterpriseArchitect.Logging
+{
+    public class EaPluginNlogConfigurator
+    {
+        private EaPluginNlogConfigurator()
+        { }
+
+        public static void InitializePluginLogging(EAAPI.Repository repository, string logTabName = "System")
+        {
+            LoggingConfiguration loggingConfig = new LoggingConfiguration();
+
+            EaPluginNlogTarget eaPluginNlogTarget = new EaPluginNlogTarget(repository, logTabName);
+
+            loggingConfig.AddTarget(eaPluginNlogTarget);
+
+            loggingConfig.AddRuleForAllLevels(eaPluginNlogTarget);
+
+            LogManager.Configuration = loggingConfig;
+
+            repository.EnsureOutputVisible(logTabName);
+        }
+    }
+}
---
+++ src/MDD4All.EnterpriseArchitect.Logging/EaPluginNlogTarget.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Copyright (c) MDD4All.de, Dr. Oliver Alt
+ */
+using NLog;
+using NLog.Targets;
+using EAAPI = EA;
+
+namespace MDD4All.EnterpriseArchitect.Logging
+{
+    public class EaPluginNlogTarget : TargetWithLayout
+    {
+        private EAAPI.Repository _repository;
+        private string _logTabName = "System";
+
+        public EaPluginNlogTarget(EAAPI.Repository repository, string logTabName)
+        {
+            Name = "EA Logger " + logTabName;
+            _repository = repository;
+            _logTabName = logTabName;
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string message = logEvent.TimeStamp.ToString() + " [" + logEvent.Level + "] " + logEvent.Message; 
+
+            if (_repository != null)
+            {
+                _repository.WriteOutput(_logTabName, message, 0);
+            }
+        }
+    }
+}
